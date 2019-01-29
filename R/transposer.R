83743e16a47880e3f083ab6a6e5f8b9c4e6a113e
Francisco J Viciana <viciana@us.es>
2019-01-29 10:05:46 +0100

    Inicial commit. Very preliminal version

---
+++ R/transposer.R
@@ -0,0 +1,92 @@
+#' transposer
+#'
+#'    Funcion principal para importar un conjunto de tablas presentes
+#'    en  .GlobalEnv sobre una estructura IDS
+#'
+#' @param file.definition xlxs con las especificaciones de la expresión de importanción
+#'        relativas a los objetos data.table que deben de estar previamente
+#'        presentes en .GlobalEnv
+#'
+#' @param sheet nombre de la hoja dentro del libro
+#'
+#' @param db.filename (aun no implemetado, DB SQLite donde se almacenara
+#'        la estructura IDS)
+#' @param Name.DataBase de la BD en IDS
+#'
+#' @importFrom data.table .N :=
+#'
+#' @export
+#'
+#' @return  outcome  una lista con las tablas INDIVIDUAL, CONTEXT, ...
+#'          con los resultados de la carga  (en versiones porterires se alamacenara
+#'          sobre una base de datos SQLite)
+#'
+transposer <- function(file.definition  = 'EntityRelationDefinition.xls',
+                       sheet = 'Entity',
+                       Name.DataBase = 'DB release_0.1',
+                       db.filename   = 'NA') {
+
+  data.table::as.data.table(readxl::read_xlsx(file.definition,
+                                                 sheet = sheet)) -> EAVs
+
+  #-------------------------------------
+  ## Carga  de datos ..
+  #-------------------------------------
+
+  # data(ids.skeleton)
+
+  ### Crea contenedores tablas receptores
+  INDIVIDUAL <- ids.skeleton$INDIVIDUAL
+  CONTEX     <- ids.skeleton$CONTEX
+
+  ### Bucle de carga
+  for (i in 1:length(EAVs[,1][[1]]) ) { print (i)
+    EAV <- EAVs[1]
+    table.source  <- get(EAV$tableName)
+    table.reciver <- get(EAV$entityType,ids.skeleton)
+
+    ### En fase de depuracion ..
+    # print(EAV$tableName)
+    # print(EAV$ValueExpression)
+    # print(EAV$DateExpression)
+    ### Solo  se definen las variables columnas que se van  a rellenar,
+    ### No es necesario definir las columnas que no recibiran datos y se rellenan a NA por defecto.
+    if (EAV$entityType == 'INDIVIDUAL') {
+      table.source[,{c(list( Id_D       = Name.DataBase ,
+                             Id_I       = eval(parse(text=EAV$entityID)),
+                             Source     = EAV$Source,
+                             Type       = EAV$Type,
+                             Value      =  eval(parse(text=EAV$ValueExpression)),
+                             Date_type  = EAV$Date_type,
+                             Estimation = EAV$DateEstimationType,
+                             Missing  =   EAV$DateMissingType),
+                       eval(parse(text=EAV$DateExpression)))}]    -> pp ;
+      pp <- rbind(pp,table.reciver,fill= TRUE)
+      INDIVIDUAL <- rbind(INDIVIDUAL,pp)
+
+    } else if (EAV$entityType == 'CONTEX') {
+      table.source[,{c(list( Id_D       = Name.DataBase ,
+                             Id_C       = eval(parse(text=EAV$entityID)),
+                             Source     = EAV$Source,
+                             Type       = EAV$Type,
+                             Value      =  eval(parse(text=EAV$ValueExpression)),
+                             Date_type  = EAV$Date_type,
+                             Estimation = EAV$DateEstimationType,
+                             Missing  =   EAV$DateMissingType),
+                       eval(parse(text=EAV$DateExpression)))}]    -> pp ;
+      pp <- rbind(pp,table.reciver,fill= TRUE)
+
+      CONTEX <- rbind(CONTEX,pp)
+    }
+  }
+
+
+  ### Relleno de columnas ID: secuencia ordenada unica
+  INDIVIDUAL[,':='(ID=1:.N)]
+  CONTEX[,':='(ID=1:.N)]
+
+  outcome <- list (INDIVIDUAL=INDIVIDUAL,CONTEX=CONTEX)
+
+  return(outcome)
+}
+
